671fab520c02c03c669d0c564368a160f949bb65
Örivelton Cesar <oconceicao@recordtv.com.br>
2019-05-12 07:53:29 -0300

    chore: Added method

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ class Car {
     this.color = color;
   }
 
-  carStart() {
+  carStarts() {
     return `This car has ${this.doors} doors, a ${this.engine} engine an a beautiful ${this.color} color!`;
+  }
+
+  static totalDoors(car1, car2) {
+    const doors1 = car1.doors;
+    const doors2 = car2.doors;
+    return doors1 + doors2;
   }
 }
 
 const cx5 = new Car(4, 'V6', 'grey');
 
 console.log(cx5);
-console.log(cx5.carStart());
+console.log(cx5.carStarts());
